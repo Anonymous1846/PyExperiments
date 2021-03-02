6d4f54023d504bf0e2c956e21404708707563d83
Sharath Sunil <alphatrinity17@gmail.com>
2021-03-02 20:24:27 +0530

    Inital Commit for calculator

---
+++ Calculator/Calculator.py
@@ -0,0 +1,10 @@
+#a simple gui calculator made with tkinter and python !
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import Button,Label
+main=tk.Tk()
+
+main.geometry('450x400')
+main.title('Calculator v1.0')
+text_field=Text(main,height=52,width=10)
+main.mainloop()
