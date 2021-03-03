5274dc804790511b9a9a206387b6577e7bc2fb9d
Sharath Sunil <alphatrinity17@gmail.com>
2021-03-03 18:09:27 +0530

    Calculator finished

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -3,21 +3,39 @@ import tkinter as tk
 from tkinter import *
 from tkinter.ttk import Button,Label
 main=tk.Tk()
-num1=num2=operator=None
+num1=num2=ans=operator=None
+answered=False
 #the geomtery stands for the width and the height later we have given the title
-main.geometry('450x400')
+main.geometry('400x350')
 main.title('Calculator v1.0')
 #the params of the calculator are root, height, width and the state(so it is readonly)
 text_field=Text(main,height=3,width=54)
 #text entry function
 #its actually number
+'''
+The function which recieves the first number num1 and stores it in the variable !
+The existing the variable is taken and the new digit is appended at the end and then the old 
+number is deleted, after that the new number is displayed on the console !
+If the query is answered earlier then it will remove the entire number and start over again !
+'''
 def enter_text(text):
-	already_text=text_field.get(1.0,"end")
-	text_field.delete(1.0,"end")
-	already_text=already_text[:-1]+text
-	text_field.insert(1.0,already_text)
+	global answered#flag to check whether equals has been pressed earlier !
+	if answered == True:
+		text_field.delete(1.0,"end")
+		text_field.insert(1.0, text)
+		answered=False
+	else:
+		already_text=text_field.get(1.0,"end")
+		text_field.delete(1.0,"end")
+		already_text=already_text[:-1]+text
+		text_field.insert(1.0,already_text)	
 text_field.pack()
+'''
+The function recieves the flag for operation and gets the second number and stores the operator information !
+Note:global variables used for avoiding conflict !
+'''
 def handle_operation(op):
+	global num1,operator,answered
 	if op=='+':
 		num1=text_field.get(1.0,"end")
 		operator="+"
@@ -41,12 +59,40 @@ def handle_operation(op):
 		new_num=float(num1)/100
 		text_field.delete(1.0,"end")
 		text_field.insert(1.0,str(new_num))
-	elif op=='=':
+		answered=True#providing true since it computes the answer here it self !
+	elif op=='c':
+		text_field.delete(1.0,'end')
+	elif op=='m':
+		num1=text_field.get(1.0,'end')
+		operator='m'
+		text_field.delete(1.0,'end')
+'''
+The main function for executing the mathematical operation,except for %
+We perform the math operations,if both numbers are set !
+'''
+def equals_operation():
+	global num2,num1,ans,answered
+	if num1 != None:
 		num2=text_field.get(1.0,'end')
+		text_field.delete(1.0,'end')
 		if operator=='+':
 			ans=float(num1)+float(num2)
-			text_field.delete(1.0,'end')
 			text_field.insert(1.0,str(ans))
+		if operator=='-':
+			ans=float(num1)-float(num2)
+			text_field.insert(1.0,str(ans))
+		if operator=='x':
+			ans=float(num1)*float(num2)
+			text_field.insert(1.0,str(ans))
+		if operator=='/':
+			ans=float(num1)/float(num2)
+			text_field.insert(1.0,str(ans))
+		if operator=='m':
+			ans=int(num1.replace('\n',''))%int(num2)
+			text_field.insert(1.0,str(ans))
+		answered=True
+		
+
 #the numerical buttons !
 one=Button(main,text='1',command=lambda:enter_text("1")).place(x=10,y=65)
 four=Button(main,text='4',command=lambda:enter_text("4")).place(x=10,y=105)
@@ -57,12 +103,14 @@ four=Button(main,text='8',command=lambda:enter_text("8")).place(x=110,y=145)
 three=Button(main,text='3',command=lambda:enter_text("3")).place(x=210,y=65)
 six=Button(main,text='6',command=lambda:enter_text("6")).place(x=210,y=105)
 nine=Button(main,text='9',command=lambda:enter_text("9")).place(x=210,y=145)
+zero=Button(main,text='0',command=lambda:enter_text('0')).place(x=10,y=185)
 #operatonal buttons
-add=Button(main,text='+',command=lambda:handle_operation("+")).place(x=10,y=185)
-sub=Button(main,text='-').place(x=110,y=185)
-mul=Button(main,text='x').place(x=210,y=185)
-div=Button(main,text='/').place(x=310,y=185)
+add=Button(main,text='+',command=lambda:handle_operation("+")).place(x=10,y=225)
+sub=Button(main,text='-',command=lambda:handle_operation('-')).place(x=110,y=185)
+mul=Button(main,text='x',command=lambda:handle_operation('x')).place(x=210,y=185)
+div=Button(main,text='/',command=lambda:handle_operation('/')).place(x=310,y=105)
 per=Button(main,text='%',command=lambda:handle_operation("%")).place(x=310,y=145)
-mod=Button(main,text='mod').place(x=310,y=105)
-equals=Button(main,text="=",command=lambda:handle_operation("=")).place(x=310,y=65)
+mod=Button(main,text='mod',command=lambda:handle_operation('m')).place(x=310,y=185)
+equals=Button(main,text="=",command=lambda:equals_operation()).place(x=310,y=65)
+c=Button(main,text='C/CE',width=28,command=lambda:handle_operation('x')).place(x=110,y=225)
 main.mainloop()
